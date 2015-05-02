4a90eabde0e3f30c425560e5f1b733f81f67715f
Mateusz Konieczny <matkoniecz@gmail.com>
2015-05-02 14:46:54 +0200

    drop dead code

--- src/main/java/bee/trip/TestBeeOnPath.java
+++ src/main/java/bee/trip/TestBeeOnPath.java
@@ -16,20 +16,15 @@ public class TestBeeOnPath {
         System.out.println("Finding good path on OSM data");
         Hive hive = new Hive(100, 20, 50, 30, 100, 10, new Location(test, 4.0));
         System.out.println("\nInitial random hive");
-        //System.out.println(hive.bestMemoryMatrix);
         System.out.println(hive.bestMeasureOfQuality);
         System.out.println(hive.ToString());
 
         hive.Solve(true);
 
-
         System.out.println("\nFinal hive");
-        //System.out.println(hive.bestMemoryMatrix);
         System.out.println(hive.bestMeasureOfQuality);
         Path best = (Path) hive.bestSolution;
         best.path.generateLeafletHtmlView("test.html");
-        //System.out.println(hive.ToString());
-
 
         System.out.println("\nEnd Simulated Bee Colony");
     }
