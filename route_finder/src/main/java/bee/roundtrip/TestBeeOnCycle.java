f7e295b2a6d789f21ed8c80bc53d1f783db8dda9
RaspyVoice <RaspyVoice@RaspyVoice-PC>
2015-06-10 09:04:14 +0200

    rename generated file

--- route_finder/src/main/java/bee/roundtrip/TestBeeOnCycle.java
+++ route_finder/src/main/java/bee/roundtrip/TestBeeOnCycle.java
@@ -10,7 +10,7 @@ public class TestBeeOnCycle {
     public static void main(String[] args) throws IOException {
         OSMDataDownloader test = new OSMDataDownloader(50.05, 19.85, 0.05);
         Graph graph = new Graph(test.getCacheFilename());
-        graph.generateLeafletHtmlView("test.html");
+        graph.generateLeafletHtmlView("all-data.html");
         System.out.println("\nBegin Simulated Bee Colony algorithm\n");
         System.out.println("Finding good path on OSM data");
         Hive hive = new Hive(20, 4, 10, 6, 100, 50, new Location(test, 10.0));
